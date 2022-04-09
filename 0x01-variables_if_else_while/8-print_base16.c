b2ff5dc816e42c4635ae0914ac14ad2cbcda78c3
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-04-08 18:06:27 -0700

    distance

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
  */
 int main(void)
 {
-	int d;
-	char low;
+	char c = 'z';
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
-		putchar(low);
-		putchar('\n');
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
 
+	putchar('\n');
 	return (0);
 }
