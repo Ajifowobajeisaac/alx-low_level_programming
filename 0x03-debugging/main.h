e0608855589e1e7db61ab396994e03aa9de02f8e
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-04-13 16:16:50 -0700

    slaying giants

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,16 +4,4 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-{
-    	int n;
-		
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-                printf("%i is positve\n", n);
-        else if (n < 0)
-                printf("%i is negative\n", n);
-        else
-                printf("%i is zero\n", n);
-        return (0);        
 #endif /* MAIN_H */
