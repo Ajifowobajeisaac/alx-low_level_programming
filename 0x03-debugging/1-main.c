15ef5e358fb7d46cadd236e8c6c309d9251fb240
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-04-13 17:36:16 -0700

    climbing mountains

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
 * main - causes an infinite loop
 * Return: 0
@@ -11,7 +13,7 @@ int main(void)
 
         i = 0;
 
-      /*  while (i < 10)*/
+        while (i < 10)
         {
                 putchar(i);
         }
