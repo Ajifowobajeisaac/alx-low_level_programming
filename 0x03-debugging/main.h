4faabd1f707912fe19e6dd6aa4f1c253bcd953c6
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-04-13 17:25:19 -0700

    get the tings

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,4 +4,24 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative or zero
+ * Return 0
+ */
+
+Int main(void)
+{
+    	int n;
+		
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+                printf("%i is positve\n", n);
+        else if (n < 0)
+                printf("%i is negative\n", n);
+        else
+                printf("%i is zero\n", n);
+        return (0);        
 #endif /* MAIN_H */
