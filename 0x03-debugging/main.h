4df6a406086811e228b4998233a5ba8ec2b46208
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2022-04-14 20:08:56 -0700

    th

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,54 +4,10 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-/**
- * main - main block
- * Description: Get a random number and print the number
- * and if it is positive, negative or zero
- * Return 0
- */
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_table(int n);
 
-int main(void)
-{
-    	int n;
-		
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-                printf("%i is positve\n", n);
-        else if (n < 0)
-                printf("%i is negative\n", n);
-        else
-                printf("%i is zero\n", n);
-        return (0);        
-}
-
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-
-int largest_number(int a, int b, int c)
-{
-    int largest;
-
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
-}
 
 #endif /* MAIN_H */
