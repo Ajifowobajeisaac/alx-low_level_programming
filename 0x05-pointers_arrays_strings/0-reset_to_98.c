fb8fc3339fd678eac391a938fba805ff69e92303
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-04-21 14:56:00 -0700

    I clearly need more understanding of this

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - check code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 402;
+
+	putchar("%p\n", &n);
+
+	*p = 98;
+
+	putchar("%d\n", &p);
+	return (0);
+}
