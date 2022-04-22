62d650b9b38998d682734b70f78918963fecfeb0
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-04-21 20:18:41 -0700

    Swapping variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main"
+
+
+/**
+ * swap_int - swaps values of 
+ * a: variable 1.
+ * b: variable 2.
+ * Return: (0).
+ */
+
+
+
+
+
+void swap_int(int *a, int *b)
+{
+	int a;
+	int b;
+	*a = &b;
+	*b = &a;
+}
