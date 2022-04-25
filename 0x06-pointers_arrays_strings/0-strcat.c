4cbd98619bb751503264346bfb4d3dfbdc526b35
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-04-25 09:45:46 -0700

    test 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+
+int main(void)
+{	
+	char dest[90] = "Hello ";
+	char src[] = "world\n";
+
+	strcat(dest,src);
+
+	putchar(dest[i]);
+}
---
+++ 0x06-pointers_arrays_strings/putchar.c
@@ -0,0 +1,10 @@
+	
+	
+putchar(char [i])
+	int i
+	ch [];
+	
+	for ( i = 0; i < 9; i++)
+	{
+		putchar (ch[i]);
+	}
