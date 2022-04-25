76775bf03f92a05ce431fefc6ccc958a001a7fec
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-04-25 10:04:42 -0700

    fixed the putchar function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,22 @@
 #include <string.h>
 #include "main.h"
 
+/**
+ * main - concatenates two strings
+ * @dest string 1.
+ * @src string 2.
+ * strcat - concatenate function
+ *
+ * Return: Always 0.
+ */
+
 
 int main(void)
-{	
+{
 	char dest[90] = "Hello ";
 	char src[] = "world\n";
 
-	strcat(dest,src);
+	strcat(dest, src);
 
-	putchar(dest[i]);
+	_putchar(dest[i]);
 }
