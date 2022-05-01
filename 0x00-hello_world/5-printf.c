99c09057cd691bbc29d71f3b484644d75b9efbe4
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-05-01 01:59:51 -0700

    going back to solve unsolved tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,17 +2,15 @@
 
 
 /**
- *  main - prints a statement with printf
+ * main - prints a statement with printf
  *
- *  Return : Always 0.
+ * Return: Always 0.
  */
 
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
-	
-	
-	return 0;
-}
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
+	return (0);
+}
