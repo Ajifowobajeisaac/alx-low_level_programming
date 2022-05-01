796d52a4f725a207cebf5b21262918f58abb3ffa
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-05-01 03:32:25 -0700

    solving the past

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints size of various types
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu\n", sizeof(char));
+	printf("Size of an int: %lu\n", sizeof(int));
+	printf("Size of a long int: %lu\n", sizeof(long int));
+	printf("Size of a long long int: %lu\n", sizeof(long long int));
+	printf("Size of a float: %lu\n", sizeof(float));
+
+	return (0);
+}
