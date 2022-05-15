dd2827773bd2e510aba21a97256533ed950d54eb
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-05-15 13:06:17 -0700

    redo

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(int __attribute__((unused)) argc,char* argv[])
+/**
+ * main - prints name of program.
+ * @argc: counter for arguments.
+ * @argv: pointer of arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-int main(int __attribute__((unused)) argc,char* argv[]);
+int main(int __attribute__((unused)) argc, char *argv[]);
 
 #endif
