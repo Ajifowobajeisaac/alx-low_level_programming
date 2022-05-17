8712e702c1f2350cc6d31b96f1763a551813287d
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-05-16 21:57:57 -0700

    I created a variadic function that prints strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings.
+ * @separator: separates strings.
+ * @n: number of string arguments.
+ * Retutn: Always 0.
+ */
+
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	
+	for (i = 0; i < n; i++)
+	{
+	
+		if (va_arg(ap, char *))
+			printf("%s", va_arg(ap, char *));
+
+		
+		if (!(separator == NULL) && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
