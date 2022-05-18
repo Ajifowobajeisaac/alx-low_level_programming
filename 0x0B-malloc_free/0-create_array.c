506b642a84165a208f0dbc97bc22a7592d80dec7
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-05-18 06:24:23 -0700

    used malloc to allocate space for an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * create_array - creates and array and initializes it.
+ * @c: character to be initialized.
+ * @size: size of the array.
+ * Return: c.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *tmp;
+
+	if (size == 0)
+		return (NULL);
+
+	tmp = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		tmp[i] = c;
+	}
+	if (tmp == NULL)
+		return (NULL);
+	else
+		return (tmp);
+}
