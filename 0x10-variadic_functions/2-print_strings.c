31edb602e7244d1ea6fb7063f6acead5e0df491c
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-05-21 17:58:43 -0700

    fixed string prints with variadic function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,12 +14,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-
+	char *c;
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, char *));
+		c = va_arg(ap, char *);
+		if (c != NULL)
+			printf("%s", c);
+		else
+			printf("NIL");
 		if (!(separator == NULL) && i < n - 1)
 			printf("%s", separator);
 	}
