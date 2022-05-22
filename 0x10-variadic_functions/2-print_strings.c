f018ad24e3813c999433bdc2813f0c7189c0d075
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-05-21 17:12:16 -0700

    fixed impcomplete string prints with variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,14 +16,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
-	
-		if (va_arg(ap, char *))
-			printf("%s", va_arg(ap, char *));
-
-		
+		printf("%s", va_arg(ap, char *));
 		if (!(separator == NULL) && i < n - 1)
 			printf("%s", separator);
 	}
