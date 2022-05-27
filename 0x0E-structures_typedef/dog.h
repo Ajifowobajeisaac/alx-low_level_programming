738cc76ad63031729250f83ccb51e96b5947b570
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-05-27 00:15:42 -0700

    Using Structs to group data types

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_
+
+/**
+ * struct dog - struct containinf details of dogs.
+ * @name:  name of dog.
+ * @owner: owner of dog.
+ * @age: age of the dog.
+ */
+
+
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif
