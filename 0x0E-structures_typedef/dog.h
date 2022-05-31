6aef06c99a8831cd9f84748f747213e54985644e
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-05-31 05:48:37 -0700

    used typedef to give stuct dog a new name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,13 +10,14 @@
 
 
 
-typedef struct dog_t
+struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-}dog_t;
+}dog;
 
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
