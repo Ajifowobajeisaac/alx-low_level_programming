606788acd8257351677e9bab0b1d398a8377064c
Ajifowobajeisaac <ajifowobjaisaac@gmail.com>
2022-07-07 21:40:26 -0700

    bulk add

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,31 +1,39 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - add node at the end of linked list.
- * @head: pointer to node.
- * @n: node data
+ * add_dnodeint_end - adds a new node at the end
+ * of a dlistint_t list
  *
- * Return: node address.
+ * @head: head of the list
+ * @n: value of the element
+ * Return: the address of the new element
  */
-
-
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *ptr;
+	dlistint_t *h;
+	dlistint_t *new;
 
-	ptr = malloc(sizeof(dlistint_t));
-
-	if (ptr == NULL)
-	{
-		free(ptr);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new;
+	}
+	else
+	{
+		*head = new;
 	}
 
-	ptr->n = n;
-	ptr->next = NULL;
-	ptr->prev = *head;
-	ptr = *head;
+	new->prev = h;
 
-	return (ptr);
+	return (new);
 }
